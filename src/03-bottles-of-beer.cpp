37de83334f5ecf87263ad4e84a49da103f6bc74f
jakubKonkol <s24406@pjwstk.edu.pl>
2021-10-19 17:39:40 +0200

    zajecia 3 - dom

--- src/03-bottles-of-beer.cpp
+++ src/03-bottles-of-beer.cpp
@@ -6,7 +6,8 @@ auto main(int argc, char* argv[]) -> int
     if (argc == 0) {
         return 1;
     }
-    int x = 1
+    int x = 1;
+    int z = 5;
     auto const Bottles = std::string{argv[1]};
     std::cout << Bottles;
 
