b0e827eed4ba03a9b72a87144d951ac796761a69
jakubKonkol <s24406@pjwstk.edu.pl>
2021-10-26 10:26:22 +0200

    zajecia 4 - dom

--- src/03-bottles-of-beer.cpp
+++ src/03-bottles-of-beer.cpp
@@ -3,11 +3,22 @@
 
 auto main(int argc, char* argv[]) -> int
 {
-    if (argc == 0) {
+    if (argc == 1) {
         return 1;
     }
-    int x = 1;
-    int z = 5;
-    auto const Bottles = std::string{argv[1]};
-    std::cout << Bottles + 2;
-
+    else{
+    
+    int y = std::stoi(argv[1]);
+    if(y<0){
+        std::cout << "Number is negative\n";
+    }
+    else{
+    do{
+        std::cout << y << " bottles of beer on the wall, " << y << " bottles of beer.\n";
+        y--;
+        std::cout << "Take one down, pass it aroud, " << y << " bottles of beer on the wall...\n";
+    }while(y>0);
+    std::cout << "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store an but some more, 99 bottles of beer on the wall...\n";
+    }    }        
+    return 0;
+}
