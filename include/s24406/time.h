199aaaf987fd32d007174a5e9a1d0743eefbbe64
jakubKonkol <s24406@pjwstk.edu.pl>
2021-11-15 17:45:08 +0100

    zadanie time skonczone!!

--- include/s24406/time.h
+++ include/s24406/time.h
@@ -8,6 +8,8 @@ struct Time{
         wieczor,
         noc
     };
+    Time() = default;
+    Time(int,int,int);
     int HH;
     int MM;
     int SS;
@@ -18,5 +20,15 @@ struct Time{
     auto next_minute() -> void;
     auto next_second() -> void;
     auto IsValid() const -> bool;
+    auto operator + (Time const&) const -> Time;
+    auto operator - (Time const&) const -> Time;
+    auto operator < (Time const&) const -> bool;
+    auto operator > (Time const&) const -> bool;
+    auto operator == (Time const&) const -> bool;
+    auto operator != (Time const&) const -> bool;
+    auto count_seconds() const -> std::uint64_t;
+    auto count_minutes() const -> std::uint64_t;
+    auto time_to_midnight() const -> Time;
+
 
 };
--- src/04-time.cpp
+++ src/04-time.cpp
@@ -36,7 +36,7 @@ auto Time::to_string(time_of_day) -> std::string{
         return "cos nie tak";
     }
 } 
-// funkcje next itp
+// funkcje next_hour/min/sec
 auto Time::next_hour() -> void{
     if(HH < 23){
         //HH=00 MM=00 SS=00
@@ -64,7 +64,7 @@ auto Time::next_second() -> void{
     }
     return;
 }
-
+//to_string
 auto Time::to_string() const -> std::string{
 
     if (Time::IsValid()==true){
@@ -103,6 +103,149 @@ auto Time::to_string() const -> std::string{
     
     return "";
 }
+//operatory
+Time::Time(int h, int m, int s){
+    HH = h;
+    MM = m;
+    SS = s;
+}
+auto Time::operator+ (Time const& o) const -> Time{
+    int h = HH;
+    int m = MM;
+    int s = SS;
+    
+    s += o.SS;
+    if(s > 59){
+        s  -= 60;
+        m++;
+    }
+    m += o.MM;
+    if(m>59){
+        m -= 60;
+        h++;
+    }
+    h += o.HH;
+    if(h > 23){
+        h -=24;
+    }
+    
+    return Time(h, m, s);
+}
+auto Time::operator- (Time const& o) const -> Time{
+    int h = HH;
+    int m = MM;
+    int s = SS;
+    s -= o.SS;
+    if(s < 0){
+        s+= 60;
+        m--;
+    }
+    m-= o.MM;
+    if(m<0){
+        m += 60;
+        h--;
+    }
+    h -= o.HH;
+    if(h<0){
+        h+=24;
+    }
+    return Time(h,m,s);
+}   
+auto Time::operator< (Time const& o) const -> bool{
+    if (HH < o.HH){
+        return true;
+    }
+    if(HH != o.HH){
+        return false;
+    }
+    if(MM < o.MM){
+        return true;
+    }
+    if(MM !=o.MM){
+        return false;
+    }
+    if(SS < o.SS){
+        return true;
+    }
+    if(SS != o.SS){
+        return false;
+    }
+    else{
+        return false;
+    }
+}
+auto Time::operator> (Time const& o) const -> bool{
+    if(HH> o.HH){
+        return true;
+    }
+    if(HH != o.HH){
+        return false;
+    }
+    if(MM > o.MM){
+        return true;
+    }
+    if(MM != o.MM){
+        return false;
+    }
+    if(SS> o.SS){
+        return true;
+    }
+    if(SS != o.SS){
+        return false;
+    }
+    else{
+        return false;
+    }
+}
+auto Time::operator== (Time const& o) const -> bool{
+    if(HH == o.HH && SS==o.SS && MM==o.MM){
+        return true;
+    }else{
+        return false;
+    }
+}
+auto Time::operator!= (Time const& o) const -> bool{
+    if(HH != o.HH && SS!=o.SS && MM!=o.MM){
+        return true;
+    }
+    else{
+        return false;
+    }
+} 
+//time_to_midnight
+auto Time::count_seconds() const -> std::uint64_t{
+    std::uint64_t seconds = 0;
+    seconds += HH * 60 * 60;
+    seconds += MM *60;
+    seconds += SS;
+    return seconds;
+}
+auto Time::count_minutes() const -> std::uint64_t{
+    std::uint64_t minutes = 0;
+    minutes += HH  *60;
+    minutes += MM;
+    return minutes;
+}
+auto Time::time_to_midnight() const -> Time{
+    Time time;
+    time.SS = 60-SS;
+
+    if(time.SS > 0){
+        time.MM = 59 - MM;
+    }
+    else{
+        time.MM = 60-MM;
+    }
+
+    if(time.MM> 0){
+        time.HH = 23 - HH;
+    }
+    else{
+        time.MM = 24-MM;
+    }
+    return time;
+}
+//main()
 auto main() -> int {
     //uzycie Time nazwa = {godz, min, sek}
     Time TestTime = {0, 0, 0};
@@ -113,11 +256,20 @@ auto main() -> int {
     std::cout << "Godzina: " << TestTime.to_string();
     TestTime.next_second();
     std::cout << "Godzina: " << TestTime.to_string();
+    std::cout <<"\n";
     std::cout << "Pora dnia: " << TestTime.to_string(Time::time_of_day()) << "\n";
+    std::cout <<"\n";
+    auto czas = Time(22, 30, 35);
+    std::cout << "godzina przed dodaniem: " << czas.to_string();
+    auto dodawanie = czas + Time(1,0,30);
+    std::cout <<"godzina po dodaniu: "<< dodawanie.to_string();
+    auto czasO = Time(22, 30, 35);
+    std::cout << "godzina przed odjeciem: " << czas.to_string();
+    auto odejmowanie = czasO - Time(1,0,30);
+    std::cout << "godzina po odjeciu: " << odejmowanie.to_string(); 
+    std::cout <<"\n";
+    std::cout << "Godzina: " << TestTime.to_string();
+    std::cout << "Czas do polnocy: " << TestTime.time_to_midnight().to_string();
     
-
-    
-
-
 
 }
